0c57c326069029f6f3316324bb4ab38e1e94a7db
nejdetkadir <nejdetkadir.550@gmail.com>
2020-10-30 02:50:34 +0300

    Init vue-cli

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque esse itaque nostrum pariatur. A aliquam consectetur dolore, eligendi explicabo fugit laboriosam laborum maxime modi nostrum quaerat quos rerum ut! Ipsam!
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+</style>
